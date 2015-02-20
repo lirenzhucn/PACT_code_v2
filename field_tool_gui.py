1c28f6bce197accaa2580b0b34d2382a2b15e0b7
Liren Zhu <liren.zhu.cn@gmail.com>
2015-02-20 10:58:01 -0600

    Implement unpack function in field tool script.

--- field_tool_gui.py
+++ field_tool_gui.py
@@ -7,6 +7,7 @@ This is a script specifically designed to be used during experiment.
 
 from PyQt4 import QtCore, QtGui
 from queue import Queue
+from ring_pact_reconstruction import Options
 
 
 class LogListener(QtCore.QThread):
@@ -47,10 +48,46 @@ class OutLogger:
         pass
 
 
+from ring_pact_reconstruction import Unpack
+
+
 class UnpackThread(QtCore.QThread):
-    """
-    Worker thread for unpack data
-    """
+    """ Worker thread for unpack data """
+
+    UNPACK_OPTS_DICT = {
+        'BoardName': ['Board2004', 'Board9054'],
+        'NumBoards': 2,
+        'DataBlockSize': 1300,
+        'PackSize': 8192,
+        'NumDaqChnsBoard': 32,
+        'TotFirings': 8,
+        'NumElements': 512,
+        'NumSegments': 1,
+        'BadChannels': [21, 22, 23, 24, 85, 86, 87, 88],
+    }
+
+    def __init__(self, srcDir='', ind=-1):
+        # super().__init__(self)
+        QtCore.QThread.__init__(self)
+        self.opts = Options(self.UNPACK_OPTS_DICT)
+        self.opts.src_dir = srcDir
+        self.opts.EXP_START = ind
+        self.opts.EXP_END = ind
+        self.opts.NUM_EXP = -1
+
+    def setParameters(self, srcDir, ind):
+        self.opts.src_dir = srcDir
+        self.opts.EXP_START = ind
+        self.opts.EXP_END = ind
+        self.opts.NUM_EXP = -1
+
+    def run(self):
+        self.unpack = Unpack(self.opts)
+        self.unpack.unpack()
+
+
+class ReconstructThread(QtCore.QThread):
+    """Worker thread for reconstruct image"""
     pass
 
 from ui_fieldToolDialog import Ui_FieldToolDialog
@@ -63,6 +100,7 @@ class MainDialog(QtGui.QDialog, Ui_FieldToolDialog):
         self.setupUi(self)
         self.configUi()
         self.setupCommunication()
+        self.setupThreads()
         self.unpackOpts = {}
         self.reconOpts = {}
 
@@ -79,6 +117,14 @@ class MainDialog(QtGui.QDialog, Ui_FieldToolDialog):
         self.mBtnClearLog.clicked.connect(self.onClearLog)
         self.mBtnChooseDataFolder.clicked.connect(self.onChooseDataFolder)
         self.mBtnClose.clicked.connect(self.onClose)
+        self.mBtnUnpack.clicked.connect(self.onUnpack)
+
+    def setupThreads(self):
+        # create threads
+        self.unpackThread = UnpackThread()
+        # connect threads' signals to slots
+        self.unpackThread.terminated.connect(self.onWorkerTerminated)
+        self.unpackThread.finished.connect(self.onWorkerDone)
 
     def setupLogger(self, logger):
         """
@@ -108,6 +154,22 @@ class MainDialog(QtGui.QDialog, Ui_FieldToolDialog):
         pathStr = QtGui.QFileDialog.getExistingDirectory(
             self, 'Data folder', pathStr)
         self.mEdDataFolder.setText(pathStr)
+
+    @QtCore.pyqtSlot()
+    def onUnpack(self):
+        """Unpack button clicked"""
+        srcDir = self.mEdDataFolder.text()
+        ind = int(self.mEdIndex.text())
+        self.unpackThread.setParameters(srcDir, ind)
+        self.unpackThread.start()
+
+    @QtCore.pyqtSlot()
+    def onWorkerDone(self):
+        print('Worker thread finished.')
+
+    @QtCore.pyqtSlot()
+    def onWorkerTerminated(self):
+        print('Worker thread terminated.')
 
 
 if __name__ == '__main__':
